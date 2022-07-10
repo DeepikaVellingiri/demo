3cc30e8140a7d1ba9e4bf3e32094727ce52acebe
DeepikaVellingiri <109020932+DeepikaVellingiri@users.noreply.github.com>
2022-07-11 03:42:06 +0530

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -3,8 +3,13 @@ function person(name):
  console.log('Name is:'+name);
 }
 
-let x=10;
-let y=20;
-x &&=y;
+let x=10
+let y=20
+x &&=y
 console.log(x)
 
+let x = func(4, 3);  
+function func(a, b)
+ {
+  return a * b; 
+ }
