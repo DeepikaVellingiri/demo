3acd8188a5ffef876e2bc414279a0c8cd920406a
DeepikaVellingiri <109020932+DeepikaVellingiri@users.noreply.github.com>
2022-07-11 11:06:26 +0530

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -15,3 +15,20 @@ function func(a, b)
   return a * b; 
  }
  
+let hour=10;
+if(hour>= 6 && hour<=12)
+{
+console.log("Good morning");
+}
+else if(hour>12 && hour<=15)
+{
+console.log("Good afternoon");
+}
+else if(hour>15 && hour<=18)
+{
+console.log("Good evening")
+}
+else
+{
+console.log("Good night")
+}
